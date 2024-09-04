1cb3347fc01ab60e3efe3438e390bdde126ca660
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-09-03 18:58:50 -0700

    updated postlist look

--- components/PostList.js
+++ components/PostList.js
@@ -8,9 +8,9 @@ const PostList = ({ posts }) => {
       {posts.map((post) => (
         <li key={post.slug}>
           
-          <Link href={`/posts/${post.slug}`} className="gap-4">
+          <Link className="space-y-6" href={`/posts/${post.slug}`} >
           {post.date && <span className="date-numerical hover:underline">{post.date}</span>}
-              {post.longdate && <span className="date-written font-bold hover:underline">{post.longdate}</span>}
+              
           </Link>
         </li>
         
