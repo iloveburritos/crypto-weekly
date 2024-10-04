0297ef2b7a69196b929fc557a0de59133b8a613f
iloveburritos <105968438+iloveburritos@users.noreply.github.com>
2024-10-04 15:17:10 -0700

    google tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,45 @@
 // pages/_app.js
 
-import '../styles/globals.css'; 
-import { Analytics } from "@vercel/analytics/react"
+import '../styles/globals.css';
+import { Analytics } from "@vercel/analytics/react";
+import Script from 'next/script';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-JEPQG3CL5W', {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-JEPQG3CL5W`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JEPQG3CL5W');
+          `,
+        }}
+      />
       <Analytics />
       <Component {...pageProps} />
     </>
